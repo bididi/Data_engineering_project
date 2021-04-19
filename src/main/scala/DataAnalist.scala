36eedede9c4999a1eeb10193a2d6ac89d08d5a8a
Gonnot <vianney.gonnot@efrei.net>
2021-04-19 23:29:36 +0200

    Requetes SQL 1 par personne

--- src/main/scala/DataAnalist.scala
+++ src/main/scala/DataAnalist.scala
@@ -24,14 +24,14 @@ object DataAnalist {
     df.show()
 
     println("Question 1 : Peacescore moyen par drone ")
-    val drone = sparkSession.sql("SELECT Id, ((AVG(Peacescore1)+AVG(Peacescore2)+AVG(Peacescore3))/3) AS score from data GROUP BY Id order by score DESC ").show()
+    val drone = sparkSession.sql("SELECT Id, ((AVG(Peacescore1)+AVG(Peacescore2)+AVG(Peacescore3))/3) AS Score_moyen from data GROUP BY Id order by Score_moyen DESC ").show()
 
     println("Question 2 : Le mot qui est le plus entendue")
-    val repetitif = sparkSession.sql("select Word, count (Word) as value from data group by Word order by value desc").show(1)
+    val repetitif = sparkSession.sql("select Word, count (Word) as Number_Word from data group by Word order by Number_Word desc").show(1)
 
 
     println("Question 3 : La personne avec le plus souvent un peacescore en dessous de 50")
-    val peace = sparkSession.sql("Select * from ((Select Name1, count(Peacescore1)as neg1 from data where(Peacescore1<50) GROUP by Name1) as tab1 full join (Select Name2, count(Peacescore2)as neg2 from data where(Peacescore2<50) GROUP by Name2) as tab2 ON tab1.Name1 = tab2.Name2 full join (Select Name3 , count(Peacescore3)as neg3 from data where(Peacescore3<50) GROUP by Name3) as tab3 ON tab2.Name2 = tab3.Name3);").show()
+    val peace = sparkSession.sql("select name , count(name) as Number_bad from (SELECT Name1 AS name, Peacescore1 from data where Peacescore1 <50 UNION ALL SELECT Name2 AS name, Peacescore2  from data where Peacescore2 <50 UNION ALL SELECT Name3 AS name, Peacescore3 from data where Peacescore3 <50) GROUP BY name order by Number_bad desc").show(1)
 
     println("Question 4 : La position Lat et Long avec le score le peascore le plus élevé")
     val geo = sparkSession.sql("SELECT Long, Lat, AVG(Peacescore1)+AVG(Peacescore2)+AVG(Peacescore3) AS score from data GROUP BY Long, Lat order by score DESC ").show(1)
