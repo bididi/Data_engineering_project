0ebaee44cfd718b840544f026a2bd9f840db085b
Gonnot <vianney.gonnot@efrei.net>
2021-04-19 22:58:58 +0200

    Requetes SQL 1 par personne

--- src/main/scala/DataAnalist.scala
+++ src/main/scala/DataAnalist.scala
@@ -1,5 +1,11 @@
 import org.apache.spark
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
+import org.apache.spark.sql.functions.asc
+import org.apache.spark.sql.functions._
+
+import scala.collection.immutable.Nil.groupBy
+
 
 object DataAnalist {
   def main(args: Array[String]): Unit = {
@@ -16,18 +22,19 @@ object DataAnalist {
     df.createOrReplaceTempView("data")
     val count = sparkSession.sql("select * from data").count()
     df.show()
-    println("Question 1 : ")
+
+    println("Question 1 : Peacescore moyen par drone ")
+    val drone = sparkSession.sql("SELECT Id, ((AVG(Peacescore1)+AVG(Peacescore2)+AVG(Peacescore3))/3) AS score from data GROUP BY Id order by score DESC ").show()
 
     println("Question 2 : Le mot qui est le plus entendue")
-    val repetitif = sparkSession.sql("select Word from data group by Word order by ").count()
-    //df.groupBy(factorCol: _*).count.agg(max(struct($"count" +: factorCol: _*)).alias("mostFrequent")).show
-    println(repetitif)
-    println("Question 3 : La personne avec le plus souvent un peacescore en 50")
-
-    println("Question 4 : La position Lat et Long le plus souvent un score enervé")
+    val repetitif = sparkSession.sql("select Word, count (Word) as value from data group by Word order by value desc").show(1)
 
 
-    sparkSession.stop()
+    println("Question 3 : La personne avec le plus souvent un peacescore en dessous de 50")
+    val peace = sparkSession.sql("Select * from ((Select Name1, count(Peacescore1)as neg1 from data where(Peacescore1<50) GROUP by Name1) as tab1 full join (Select Name2, count(Peacescore2)as neg2 from data where(Peacescore2<50) GROUP by Name2) as tab2 ON tab1.Name1 = tab2.Name2 full join (Select Name3 , count(Peacescore3)as neg3 from data where(Peacescore3<50) GROUP by Name3) as tab3 ON tab2.Name2 = tab3.Name3);").show()
+
+    println("Question 4 : La position Lat et Long avec le score le peascore le plus élevé")
+    val geo = sparkSession.sql("SELECT Long, Lat, AVG(Peacescore1)+AVG(Peacescore2)+AVG(Peacescore3) AS score from data GROUP BY Long, Lat order by score DESC ").show(1)
   }
 
 }
